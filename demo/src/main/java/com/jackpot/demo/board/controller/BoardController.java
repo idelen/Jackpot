1694162a84e4e670c79566abe8d8b93a569ea510
idelen <ch.jackpot@gmail.com>
2021-09-12 20:30:14 +0900

    [

---
+++ demo/src/main/java/com/jackpot/demo/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.jackpot.demo.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+    @RequestMapping("")
+    public String home() {
+        return "board/home";
+    }
+
+}
